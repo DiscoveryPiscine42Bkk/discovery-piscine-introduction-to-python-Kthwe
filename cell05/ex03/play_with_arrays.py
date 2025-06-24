6904b455a107cebc1c4610bee74adf49e27e6616
Kthwe <61928319+Kthwe@users.noreply.github.com>
2025-06-24 16:10:52 +0700

    Add files via upload

---
+++ cell05/ex03/play_with_arrays.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+
+new_set = set()
+for number in original_array:
+    if number > 5:
+        new_set.add(number + 2)
+
+# Display both arrays
+print(original_array)
+print(new_set)
